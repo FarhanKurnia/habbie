2d39bb03dee1c4c38cd45943ce2601bb466b29d5
bagusgandhi <bagusgandhi4@gmail.com>
2023-06-27 07:51:47 +0700

    feat: add about page route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
+    return view('pages.public.home');
+});
+
+Route::get('/about', function () {
+    return view('pages.public.about');
 });
