b052291d2e0e8b61301f1b75ed3f248419a80f2b
bagusgandhi <bagusgandhi4@gmail.com>
2023-06-28 17:36:04 +0700

    feat: change slide to slick slider

--- resources/views/components/public/partials/hero-slider.blade.php
+++ resources/views/components/public/partials/hero-slider.blade.php
@@ -1,18 +1,10 @@
-<div class="container mx-auto relative flex justify-center">
-    {{-- slider hero image --}}
-    <div class="carousel w-full" style="height: 520px;">
+<div class="container mx-auto ">
+    <div class="slider relative">
         @foreach (range(1, 4) as $item)
-            <div id="item{{ $item }}" class="carousel-item w-full">
-                <img src="{{ asset('storage/img/img-slide.jpg') }}" class="w-full object-cover" />
+            <div>
+                <img src="{{ asset('storage/img/img-slide.jpg') }}" alt="" class="w-full object-cover "
+                    style="height: 520px;">
             </div>
         @endforeach
-    </div>
-    <div class="absolute bottom-0 ">
-        {{-- slider hero control --}}
-        <div class="flex justify-center w-full py-2 gap-4">
-            @foreach (range(1, 4) as $item)
-                <a href="#item{{ $item }}" class="p-1.5 bg-grey rounded-full"></a>
-            @endforeach
-        </div>
     </div>
 </div>
