00737415422be6d79c5974c620ecd6ed9e702a8c
bagusgandhi <bagusgandhi4@gmail.com>
2023-06-28 17:41:20 +0700

    feat: adding css and js assets

---
+++ public/assets/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        fade: true,
+        cssEase: 'ease-in-out',
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: true,
+        touchMove: true,
+        swipe: true,
+        swipeToSlide: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        ]
+    });
+});
+
+$(document).ready(function () {
+
+    var contentSlider = $('#content-slider');
+    contentSlider.slick({
+        arrows: false,
+        swipe: true,
+    });
+
+    $('.prev-btn').on('click', function () {
+        contentSlider.slick('slickPrev');
+    });
+
+    $('.next-btn').on('click', function () {
+        contentSlider.slick('slickNext');
+    });
+
+
+});
