6abc494e8a47ed3c9f32aed5f84e2e339aa05031
bagusgandhi <bagusgandhi4@gmail.com>
2023-06-28 17:43:55 +0700

    feat: create card content components

---
+++ resources/views/components/public/partials/content-card.blade.php
@@ -0,0 +1,12 @@
+<div class="card lg:w-96 bg-base-100 shadow-xl">
+    <figure class="px-10 pt-10">
+        <img src="{{ asset('storage/img/img-slide.jpg') }}" alt="" />
+    </figure>
+    <div class="card-body items-center text-center">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.</p>
+        <div class="card-actions pt-10">
+            <button class="bg-transparent font-bold text-lg">Learn More</button>
+        </div>
+    </div>
+</div>
