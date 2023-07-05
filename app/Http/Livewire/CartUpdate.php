e14d927be72d6cca4473660045f7c6401ea41ed4
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-05 22:00:22 +0700

    feat: adding increment and decrement quantity item

--- app/Http/Livewire/CartUpdate.php
+++ app/Http/Livewire/CartUpdate.php
@@ -6,6 +6,41 @@ use Livewire\Component;
 
 class CartUpdate extends Component
 {
+    public $cartItems = [];
+    public $quantity = 1;
+    protected $listeners = ['updateCart'];
+
+    public function mount($item){
+        $this->cartItems = $item;
+        $this->quantity = $item['quantity'];
+    }
+
+    public function increment()
+    {
+        $this->quantity++;
+        $this->emit('updateCart');
+    }
+
+    public function decrement()
+    {
+        if($this->quantity > 1){
+            $this->quantity--;
+            $this->emit('updateCart');
+        }
+    }
+
+    public function updateCart(){
+        \Cart::update($this->cartItems['id'], [
+            'quantity' => [
+                'relative' => false,
+                'value' => $this->quantity
+            ]
+        ]);
+
+        $this->emit('cartUpdated');
+        $this->emit('addToCart');
+    }
+
     public function render()
     {
         return view('livewire.cart-update');
