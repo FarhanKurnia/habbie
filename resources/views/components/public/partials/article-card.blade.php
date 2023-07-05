e7094b0d485656e39c5cec07863ab62030d6a20d
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-05 22:09:36 +0700

    feat: creating article card

--- resources/views/components/public/partials/article-card.blade.php
+++ resources/views/components/public/partials/article-card.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <p>this is article cart</p>
+<div class="p-4">
+    <img class="py-2 rounded-xl" src="{{ $articles['image'] }}" alt="" />
+    <h4 class="text-pink-primary font-bold text-xl py-2">{{ $articles['title'] }}</h4>
+    <p>{{ $articles['post'] }}</p>
+    <a href="#" class="text-pink-primary italic">Read More</a>
 </div>
