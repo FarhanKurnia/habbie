30b23399add77bb2f87f5f4d2c81351af6d09238
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-06 08:31:34 +0700

    feat: create component livewire header Recomendation

---
+++ resources/views/livewire/header-recommendation.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-pink-bloosom">
+    <div class="announcement-bar container mx-auto">
+        <div class="flex flex-row gap-4 py-2 justify-center">
+            <p class="text-center text-sm">Aroma segar untuk kamu coba minggu ini: <span class="text-center font-bold text-pink-primary text-sm">{{ $product[0]->name }}</span></p>
+        </div>
+    </div>
+</div>
