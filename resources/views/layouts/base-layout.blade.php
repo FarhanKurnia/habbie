a69279bc16b83ce12146674e184eff1e8b7ca039
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-06 08:27:01 +0700

    feat: add cdn font

--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
-    <!-- Add the slick-theme.css if you want default styling -->
+    <link href="https://fonts.cdnfonts.com/css/dinpro-medium" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/dinpro-bold" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css" />
-    <!-- Add the slick-theme.css if you want default styling -->
     <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
     <link rel="stylesheet" type="text/css" href="/assets/css/styles.css" />
@@ -23,7 +22,7 @@
         @include('components.public.template.header')
     </header>
 
-    <main>
+    <main class="pt-24">
         @yield('content')
     </main>
 
@@ -33,10 +32,10 @@
 
     @livewireScripts
 
-    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
-    <script type="text/javascript" src="assets/js/script.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
+    <script type="text/javascript" src="{{ url('/assets/js/script.js'); }}"></script>
 </body>
 
 </html>
