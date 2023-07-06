5cb639724e66753bf218c90057901227d42e9cdd
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-06 08:21:28 +0700

    feat: change content slider

--- resources/views/components/public/partials/content-slider.blade.php
+++ resources/views/components/public/partials/content-slider.blade.php
@@ -1,16 +1,16 @@
 <div class="container mx-auto">
-    <div class="relative lg:w-3/4 lg:mx-auto">
+    <div class="relative">
         <div id="content-slider" class="py-14">
             @foreach ($products as $product)
                 <div>
-                    <div class="w-2/3 mx-auto lg:grid lg:grid-cols-2 lg:gap-10 items-center">
-                        <div class="image-content pb-4">
-                            <img src="{{ asset('storage/img/img-slide.jpg') }}" alt="Slide Image" />
+                    <div class="w-2/3 mx-auto lg:grid lg:grid-cols-2 lg:gap-10 items-center space-y-10">
+                        <div class="image-content h-64">
+                            <img class="max-h-64 mx-auto" src="{{ $product->image }}" alt="{{ $product->name }}" />
                         </div>
                         <div class="text-content">
                             <p class="text-gray-400 text-sm text-grey">RECOMMENDATION FOR YOU</p>
-                            <h3 class="text-2xl font-bold text-pink-primary">{{ $product['title'] }}</h3>
-                            <p class="pb-4">{{ $product['description'] }}</p>
+                            <h3 class="text-xl font-bold text-pink-primary">{{ $product->name }}</h3>
+                            <p class="pb-4">{{ $product->description }}</p>
                             <button class="btn btn-primary rounded-full font-bold text-white">Shop Now</button>
                         </div>
                     </div>
