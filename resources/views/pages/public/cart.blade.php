a1b4fd90f24b97b32b4ebc10a06e475de6cabbfa
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-06 08:28:33 +0700

    feat: add cart list livewire component

---
+++ resources/views/pages/public/cart.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base-layout')
+
+@section('title', 'Cart')
+@section('content')
+
+    <div class="container mx-auto pt-14 px-6 lg:px-0">
+        <livewire:cart-list />
+
+    </div>
+@endsection
