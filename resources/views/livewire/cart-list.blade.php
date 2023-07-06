cb8a75a4a0ea2b610611aad8006095d03c8e92b1
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-06 08:24:20 +0700

    feat: update livewire  cart

--- resources/views/livewire/cart-list.blade.php
+++ resources/views/livewire/cart-list.blade.php
@@ -1,36 +1,60 @@
 <div class="container mx-auto">
-    @if ($message = Session::get('success'))
-        <div class="bg-green p-4 rounded">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
 
-    <div class="w-2/3 mx-auto py-10">
+    <div class="lg:w-3/4 mx-auto pb-10">
+        <h3 class="text-grey pb-4">Produk yang ada di keranjang belanja</h3>
         <div class="overflow-x-auto">
             <table class="table">
-                <thead class="bg-grey-secondary">
-                    <tr class="text-sm font-bold text-center">
+                <thead class="bg-grey-secondary-50">
+                    <tr class="text-sm font-bold">
                         <th></th>
                         <th>PRODUCT</th>
-                        <th>QTY</th>
                         <th>PRICE</th>
+                        <th>QTY</th>
                         <th>TOTAL PRICE</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($cartItems as $item)
-                        <tr class="text-center">
-                            <td></td>
-                            <td>{{ $item['name'] }}</td>
-                            <td>{{ \App\Helpers\CurrencyUtils::formatCurrency($item['price']) }}</td>
-                            <td class="justify-center flex gap-3 items-center">
+                        <tr class="text-left">
+                            <td>
+                                <span class="p-2 hover:text-pink-primary cursor-pointer"
+                                    wire:click="removeCart('{{ $item['id'] }}')">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                    </svg>
+
+                                </span>
+                            </td>
+                            <td class="cursor-pointer">
+                                <a href="{{ url('products/' . $item['attributes']->slug) }}" class="flex flex-row gap-10 items-center">
+                                    <span>
+                                        <img class="h-20" src="{{ url($item['attributes']->image) }}"
+                                            alt="{{ $item['name'] }}">
+                                    </span>
+                                    <span class="hover:text-pink-primary">{{ $item['name'] }}</span>
+                                </a>
+                            </td>
+                            <td>{{ \App\Helpers\CurrencyFormat::data($item['price']) }}</td>
+                            <td class="items-center">
                                 <livewire:cart-update :item="$item" :key="$item['id']" />
                             </td>
-                            <td>{{ $item['price'] * $item['quantity'] }}</td>
+                            <td>{{ \App\Helpers\CurrencyFormat::data($item['price'] * $item['quantity']) }}</td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+            <div class="p-6 bg-grey-secondary-50 w-1/3 ml-auto rounded">
+                <div class="flex flex-row items-center justify-between">
+                    <p class="text-xl">Total: </p>
+                    <p class="text-2xl font-bold pr-2">{{ \App\Helpers\CurrencyFormat::data((Cart::getTotal())) }}</p>
+                </div>
+                <div class="text-right pt-6">
+                    <button class="btn btn-primary rounded-full text-base-100">Checkout</button>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
--- resources/views/livewire/cart-update.blade.php
+++ resources/views/livewire/cart-update.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <input class="bg-grey-secondary p-2 w-10 " wire:model="quantity" type="number" min="1" wire:change="updateCart">
+<div class="flex flex-row rounded-md font-bold items-center">
+    <button wire:click="decrement" class="p-2 cursor-pointer">-</button>
+    <input type="number" wire:model="quantity" min="1" wire:change="updateCart"
+        class="py-2 focus:outline-none text-center w-full bg-grey-secondary-50 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700 outline-none">
+    <button wire:click="increment" class="p-2 cursor-pointer">+</button>
 </div>
