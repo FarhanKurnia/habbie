e6f27953f6bc8029e5c170f02fd0dfa3899a170c
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-08 16:02:26 +0700

    feat: create livewire component menu dropdown caregory

--- resources/views/livewire/menu-category-product.blade.php
+++ resources/views/livewire/menu-category-product.blade.php
@@ -1,3 +1,20 @@
-<div>
-    {{ $categories }}
-</div>
+<ul class="flex flex-row p-2 z-20">
+    <li>
+        <ul class="flex flex-col">
+            @foreach ($categories as $index => $category)
+                @if ($index % 2 === 0)
+                    <li><a href="{{ url('/products/categories/'. $category->slug) }}">{{ $category->name }}</a></li>
+                @endif
+            @endforeach
+        </ul>
+    </li>
+    <li>
+        <ul class="flex flex-col">
+            @foreach ($categories as $index => $category)
+                @if ($index % 2 !== 0)
+                    <li><a href="{{ url('/products/categories/'. $category->slug) }}">{{ $category->name }}</a></li>
+                @endif
+            @endforeach
+        </ul>
+    </li>
+</ul>
