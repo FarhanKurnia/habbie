919fab3d4aa357467ea7314ddbef0513d6acaffb
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-08 11:11:53 +0700

    feat: change bg color input

--- resources/views/livewire/cart-update.blade.php
+++ resources/views/livewire/cart-update.blade.php
@@ -1,6 +1,6 @@
-<div class="flex flex-row rounded-md font-bold items-center">
-    <button wire:click="decrement" class="p-2 cursor-pointer">-</button>
+<div class="flex flex-row rounded-md font-bold items-center text-md lg:text-md">
+    <button wire:click="decrement" class="lg:p-2 px-4 cursor-pointer">-</button>
     <input type="number" wire:model="quantity" min="1" wire:change="updateCart"
-        class="py-2 focus:outline-none text-center w-full bg-grey-secondary-50 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700 outline-none">
-    <button wire:click="increment" class="p-2 cursor-pointer">+</button>
+        class="py-2 focus:outline-none text-center w-full bg-grey-secondary-50 rounded-md font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700 outline-none">
+    <button wire:click="increment" class="lg:p-2 px-4 cursor-pointer">+</button>
 </div>
