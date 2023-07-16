fd25e66dfdf653eced83e07fdd916d863c0d3fe3
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-17 00:56:00 +0700

    feat: Insert data to Order and OrderProducts table

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -27,10 +27,58 @@ class OrderService {
         return $invoice;
     }
 
+    protected function insertOrder($orderData, $invoice)
+    {
+        $customer = $orderData['customer'];
+        $shipping = $orderData['shipping'];
+        $subtotal = $orderData['subtototal'];
+
+        $order = Order::create([
+            'name' => $customer['name'], 
+            'email' => $customer['email'], 
+            'phone' => $customer['phone'], 
+            'address' => $customer['address'],
+            'province' => $customer['province'],
+            'city' => $customer['city'],
+            'subdistrict' => $customer['subdistrict'],
+            'postal_code' => $customer['postal_code'],
+            'status' => 'pending', 
+            'sub_total' => $subtotal,
+            'shipping_code' => $shipping['code'], 
+            'shipping_service' => $shipping['service'],
+            'shipping_value' => $shipping['value'],
+            'shipping_etd' => $shipping['etd'],  
+            'invoice' => $invoice,
+            'user_id' => $customer['customer_id'], 
+        ]);
+
+        $id_order = $order->id_order;
+
+        return $id_order;
+    }
+
+    protected function insertOrderProduct($orderData, $orderId)
+    {
+        $products = collect($orderData['products']);
+
+        $products->map(function ($item) use ($orderId) {
+            OrderProduct::create([
+                "order_id" => $orderId,
+                "product_id" => $item['id'],
+                "qty" => $item['quantity'],
+            ]);
+        });
+    }
+
     public function create()
     {
-        // dd($this->orderData);
-        dd($this->generateInvoice());
+        $orderData = $this->orderData;
+        $invoice = $this->generateInvoice();
+        $orderId = $this->insertOrder($orderData, $invoice);
+
+        $this->insertOrderProduct($orderData, $orderId);
+
+        return $invoice;
 
     }
 }
