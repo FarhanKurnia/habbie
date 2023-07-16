10389565b203aeae9b85eea153812592b9d2a7b1
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-16 20:14:32 +0700

    feat: add slug url

--- resources/views/components/public/partials/content-list.blade.php
+++ resources/views/components/public/partials/content-list.blade.php
@@ -3,9 +3,9 @@
         <img src="{{ $offer['image'] }}" class=" w-full object-cover rounded-xl" />
     </div>
     <div>
-        <h3 class="text-2xl font-bold text-pink-primary lg:py-4">{{ $offer['title'] }}</h3>
+        <h3 class="text-2xl font-bold text-pink-primary lg:py-4">{{ $offer['name'] }}</h3>
         <p>{{ $offer['description'] }}</p>
-        <a href="#">
+        <a href="{{ url('/offers/'. $offer['slug']) }}">
             <button class="btn btn-primary rounded-full font-bold text-white my-6">Shop Now</button>
         </a>
     </div>
