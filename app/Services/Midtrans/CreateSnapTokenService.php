53b7039021169915aa25f7ef2a3cf7ea8dda2558
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-20 21:55:37 +0700

    fix: changer params to property order

--- app/Services/Midtrans/CreateSnapTokenService.php
+++ app/Services/Midtrans/CreateSnapTokenService.php
@@ -14,33 +14,8 @@ class CreateSnapTokenService extends Midtrans {
     }
 
     public function getSnapToken()
-    {
-        $params = [
-            'transaction_details' => [
-                'order_id' => $this->order['id'],
-                'gross_amount' => $this->order['subtotal'],
-            ],
-            'item_details' => array_values($this->order['products']),
-            'customer_details' => $this->order['customer']
-            // sample data customer detail 
-            // 'customer_details' => [
-            //     'first_name' => 'Martin Mulyo Syahidin',
-            //     'email' => 'mulyosyahidin95@gmail.com',
-            //     'phone' => '081234567890',
-                // "shipping_address": {
-                //     "first_name": "TEST",
-                //     "last_name": "MIDTRANSER",
-                //     "email": "test@midtrans.com",
-                //     "phone": "0 8128-75 7-9338",
-                //     "address": "Sudirman",
-                //     "city": "Jakarta",
-                //     "postal_code": "12190",
-                //     "country_code": "IDN"
-                //   }
-            // ]
-        ];
-        
-        $snapToken = Snap::getSnapToken($params);
+    {   
+        $snapToken = Snap::getSnapToken($this->order);
         return $snapToken;
     }
 }
