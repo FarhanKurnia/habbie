307194152781e9772fa9088c599a44984252c3a2
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-20 21:56:28 +0700

    feat: get data payment from db

--- resources/views/pages/public/payment.blade.php
+++ resources/views/pages/public/payment.blade.php
@@ -6,40 +6,64 @@
     <div class="container mx-auto lg:w-1/3 px-6 lg:px-0">
         <div class="my-14">
             <div class="bg-grey-secondary-50">
-                @php
-                    $cartItems = \Cart::getContent();
-                @endphp
+
                 <h3 class="font-bold text-xl p-4">Order Details</h3>
                 <ul>
-                    @foreach ($cartItems as $item)
+                    @foreach ($orderProducts as $item)
                         <li class="my-1">
-                            <a href="{{ url('products/' . $item['attributes']->slug) }}">
-                                <div class="flex bg-white p-4 items-center">
-                                    <div class="w-1/3">
-                                        <img class="h-20 mx-auto" src="{{ url($item['attributes']->image) }}"
-                                            alt="{{ $item['name'] }}">
-                                    </div>
-                                    <div>
-                                        <h4 class="font-bold text-xl">{{ $item['name'] }}</h4>
-                                        <p class="text-sm text-grey-secondary">
-                                            {{ \App\Helpers\CurrencyFormat::data($item['price']) . ' X ' . $item['quantity'] }}
-                                        </p>
-                                        <p class="font-bold">
-                                            {{ \App\Helpers\CurrencyFormat::data($item['price'] * $item['quantity']) }}</p>
-                                    </div>
+                            <div class="flex bg-white p-4 items-center">
+                                <div class="w-1/3">
+                                    <img class="h-20 mx-auto" src="{{ url($item['product']['image']) }}"
+                                        alt="{{ $item['product']['name'] }}">
                                 </div>
-                            </a>
+                                <div>
+                                    <h4 class="font-bold text-xl">{{ $item['product']['name'] }}</h4>
+
+                                    @php
+                                        $price = $item['product']['price'];
+                                            
+                                    @endphp
+
+                                    @if ($item['product']['discount'])
+                                        @php
+                                            
+                                            $discount = $item['product']['discount']['rule'];
+                                            $price = $price - (( $discount / 100 ) * $price );
+                                        @endphp
+                                    @endif
+
+                                    <p class="text-sm text-grey-secondary">
+                                        {{ \App\Helpers\CurrencyFormat::data($price) . ' X ' . $item['qty'] }}
+                                    </p>
+                                    <p class="font-bold">
+                                        {{ \App\Helpers\CurrencyFormat::data($price * $item['qty']) }}
+                                    </p>
+                                </div>
+                            </div>
                         </li>
                     @endforeach
                 </ul>
                 <div class="my-8 p-4 flex flex-col space-y-4">
                     <span class="flex justify-between">
-                        <p>Subtotal</p>
-                        <p class="font-bold">{{ \App\Helpers\CurrencyFormat::data(Cart::getTotal()) }}</p>
+                        <p>Subtotal Produk</p>
+                        <p >{{ \App\Helpers\CurrencyFormat::data($order['sub_total'] - $order['shipping_value']) }}</p>
+                    </span>
+                    <span class="flex justify-between items-center">
+                        <span>
+                            <p>Biaya Ongkir</p>
+                            <p class="text-sm text-grey-secondary">{{$order['shipping_code'] . " " . $order['shipping_service']}}</p>
+                            <p class="text-sm text-grey-secondary">Estimasi Pengiriman {{$order['shipping_etd']}} Hari</p>
+                        </span>
+                        <p class="">{{ \App\Helpers\CurrencyFormat::data($order['shipping_value']) }}</p>
+                    </span>
+                    <span class="flex justify-between">
+                        <p>Total</p>
+                        <p class="font-bold text-xl text-pink-primary">{{ \App\Helpers\CurrencyFormat::data($order['sub_total']) }}</p>
                     </span>
                 </div>
             </div>
-            <button id="pay-button" type="" class="btn btn-primary text-white rounded-full">Lanjut Pembayaran</button>
+            <button id="pay-button" type="" class="btn btn-primary text-white rounded-full">Lanjut
+                Pembayaran</button>
         </div>
 
     </div>
@@ -48,27 +72,31 @@
         data-client-key="{{ config('service.midtrans.clientKey') }}"></script>
     <script>
         const payButton = document.querySelector('#pay-button');
+
         payButton.addEventListener('click', function(e) {
             e.preventDefault();
 
+
             snap.pay('{{ $snapToken }}', {
-                // Optional
+
                 onSuccess: function(result) {
-                    /* You may add your own js here, this is just example */
-                    // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
-                    console.log(result)
+                    window.location.replace('http://localhost:8000/invoice/' + {{ $order['invoice'] }});
                 },
-                // Optional
+
                 onPending: function(result) {
-                    /* You may add your own js here, this is just example */
-                    // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
                     console.log(result)
                 },
-                // Optional
+
                 onError: function(result) {
-                    /* You may add your own js here, this is just example */
-                    // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
-                    console.log(result)
+                    // document.cookie.split
+
+                    function deleteCookie(key) {
+                        document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+                    }
+
+                    deleteCookie('{{$order['invoice']}}');
+                    
+                    window.location.replace('http://localhost:8000/cart');
                 }
             });
         });
