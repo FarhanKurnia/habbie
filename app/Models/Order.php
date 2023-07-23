372378f6d87df9f0804fa11513ee6de5a115f5c7
FarhanKurnia <itsfarhankurnia@gmail.com>
2023-07-23 22:41:55 +0700

    feat: add total in product and total_weight in order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -12,7 +12,7 @@ class Order extends Model
     protected $fillable = [
         'name', 'email', 'phone', 'address','province','city','subdistrict',
         'postal_code', 'status', 'sub_total', 'resi', 'user_id','shipping_code',
-        'shipping_service','shipping_value','shipping_etd','invoice', 'total',
+        'shipping_service','shipping_value','shipping_etd','invoice', 'total', 'total_weight'
     ];
 
     // one order has many products
