15571768cc4b51a15d3b3c98de52778ee35a0a00
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-24 22:24:38 +0700

    feat: add link product

--- resources/views/livewire/header-recommendation.blade.php
+++ resources/views/livewire/header-recommendation.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-pink-bloosom">
     <div class="announcement-bar container mx-auto">
         <div class="flex flex-row gap-4 py-2 justify-center">
-            <p class="text-center text-sm">Aroma segar untuk kamu coba minggu ini: <span class="text-center font-bold text-pink-primary text-sm">{{ $product[0]->name }}</span></p>
+            <p class="text-center text-sm">Aroma segar untuk kamu coba minggu ini: <span class="text-center font-bold text-pink-primary text-sm"><a href="{{ url('/products/' . $product[0]->slug) }}">{{ $product[0]->name }}</a></span></p>
         </div>
     </div>
 </div>
