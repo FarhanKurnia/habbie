077ff56847dfc513910247de4c101fbb3a7a2367
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-27 22:54:00 +0700

    feat: adding star rating

--- resources/views/pages/public/products/detail.blade.php
+++ resources/views/pages/public/products/detail.blade.php
@@ -14,6 +14,31 @@
                     <p class="text-grey">{{ $oneProduct->category->name }}</p>
                 </div>
 
+                <div class="flex flex-row pt-2">
+                    @php
+                        $maxRating = 5;
+                        $fillRating = $oneProduct->rating;
+                        $emptyRating = $maxRating - $fillRating;
+                    @endphp
+
+                    @for ($i = 1; $i <= $fillRating; $i++)
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2"
+                            stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" fill="#f9d800" stroke="#8c8c8c"
+                                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                        </svg>
+                    @endfor
+
+                    @for ($i = 1; $i <= $emptyRating; $i++)
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2"
+                            stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke="#8c8c8c"
+                                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                        </svg>
+                    @endfor
+
+                </div>
+
                 <div class="py-4">
                     <p>{{ $oneProduct->description }}</p>
                 </div>
