f677ca990c5f593aef6242eba257792e83c2ec09
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-27 21:54:30 +0700

    feat: adding condition badge

--- resources/views/components/public/partials/content-list.blade.php
+++ resources/views/components/public/partials/content-list.blade.php
@@ -1,12 +1,20 @@
 <div class="grid lg:grid-cols-2 gap-6 items-center">
-    <div class="">
-        <img src="{{ $offer['image'] }}" class=" w-full object-cover rounded-xl" />
-    </div>
-    <div>
-        <h3 class="text-2xl font-bold text-pink-primary lg:py-4">{{ $offer['name'] }}</h3>
-        <p>{{ $offer['description'] }}</p>
-        <a href="{{ url('/product/'. $offer['slug']) }}">
-            <button class="btn btn-primary rounded-full font-bold text-white my-6">Shop Now</button>
-        </a>
-    </div>
+    <a href="{{ url('/products/'. $offer->product['slug']) }}">
+        <div class="bg-grey-secondary-50 p-8 rounded relative">
+            @if ($index < 4)                
+            <span class="bg-pink-primary p-6 absolute left-0 top-0 rounded">
+                <p class="text-white font-bold text-xl">New</p> 
+            </span>
+            @endif
+            {{-- <p>{{$index}}</p> --}}
+            <img src="{{ $offer->product['image'] }}" class="h-60 mx-auto rounded-xl" />
+        </div>
+        <div>
+            <h3 class="text-2xl font-bold text-pink-primary lg:py-4">{{ $offer['name'] }}</h3>
+            <p>{{ $offer['description'] }}</p>
+            <a href="{{ url('/products/'. $offer->product['slug']) }}">
+                <button class="btn btn-primary rounded-full font-bold text-white my-6">Shop Now</button>
+            </a>
+        </div>
+    </a>
 </div>
