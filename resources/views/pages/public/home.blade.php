6e5b77180bb7563d13afe83f3e5ba18d12818b6b
bagusgandhi <bagusgandhi4@gmail.com>
2023-07-31 21:34:35 +0700

    feat: add url media in button read all articles

--- resources/views/pages/public/home.blade.php
+++ resources/views/pages/public/home.blade.php
@@ -48,7 +48,7 @@
             @endforeach
         </div>
         <div class="text-center py-4">
-            <button class="btn btn-primary rounded-full font-bold text-white">READ ALL ARTICLES</button>
+            <a href="{{ url('media') }}"><button class="btn btn-primary rounded-full font-bold text-white">READ ALL ARTICLES</button></a>
         </div>
     </div>
 
