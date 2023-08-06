6e77b0b1fea3eea45f825c07970ad5ac2dddabf8
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-06 11:27:36 +0700

    feat: create index careers page

---
+++ resources/views/pages/public/careers/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.base-layout')
+
+@section('title', 'Careers')
+@section('content')
+
+
+    <div class="container mx-auto pt-14 px-6">
+        @include('components.public.partials.title', [
+            'title' => 'Careers',
+            'align' => 'left',
+            'color' => 'pink-primary',
+        ])
+        <div class="space-y-14">
+            @foreach ($careers as $index => $career)
+                @include('components.public.partials.content-list-careers', ['data' => $career, $index])
+            @endforeach
+        </div>
+
+        <div class="p-4 pagination">
+            {{ $careers->links() }}
+        </div>
+    </div>
+
+    <div class="h-64 bg-pink-primary overflow-y-hidden">
+        <img src="{{ asset('storage/img/img-slide.jpg') }}" alt="" class="object-cover opacity-25 h-full w-full">
+    </div>
+
+
+
+@endsection
