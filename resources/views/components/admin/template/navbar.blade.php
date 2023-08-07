e399c23482df26cdbefbc1c4a8842cf5d75281bc
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-07 23:49:23 +0700

    feat: adding navbar menu

---
+++ resources/views/components/admin/template/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none navbar">
+    <div class="navbar-start">
+        <input type="text" placeholder="Type to search..." class="w-full bg-transparent pr-4 pl-9 focus:outline-none" />
+    </div>
+    <div class="navbar-end">
+        <ul class="menu menu-horizontal px-1">
+            <li><a>Item 1</a></li>
+            <li tabindex="0">
+              <details>
+                <summary>Parent</summary>
+                <ul class="p-2">
+                  <li><a>Profile</a></li>
+                  <li><a>Logout</a></li>
+                </ul>
+              </details>
+            </li>
+        </ul>
+    </div>
+
+</header>
