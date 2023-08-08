ffe9fd5595ad737e5cbb3bb7cc7ec6ebcd039242
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-08 22:58:20 +0700

    feat:add component livewire menu

--- resources/views/components/admin/template/navbar.blade.php
+++ resources/views/components/admin/template/navbar.blade.php
@@ -1,13 +1,12 @@
-<header class="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none navbar">
+<header class="sticky top-0 z-20 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none navbar">
     <div class="navbar-start">
         <input type="text" placeholder="Type to search..." class="w-full bg-transparent pr-4 pl-9 focus:outline-none" />
     </div>
     <div class="navbar-end">
         <ul class="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
             <li tabindex="0">
               <details>
-                <summary>Parent</summary>
+                <summary>John Doe</summary>
                 <ul class="p-2">
                   <li><a>Profile</a></li>
                   <li><a>Logout</a></li>
