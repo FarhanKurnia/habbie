11e52a7aa20da3feb9310a6a66169685c8460dfe
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-13 20:13:24 +0700

    feat: index page product admin

--- resources/views/pages/admin/products/index.blade.php
+++ resources/views/pages/admin/products/index.blade.php
@@ -3,5 +3,7 @@
 @section('title', 'Product List')
 
 @section('content')
-
+    <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 ">
+        <livewire:admin.products-table />
+    </div>
 @endsection
