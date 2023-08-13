0e641c8df70b4d32e98ff4d5cd4c8ac8c7879e85
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-13 20:15:29 +0700

    feat: index page category product

---
+++ resources/views/pages/admin/products/categories/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin-layout')
+
+@section('title', 'Product Category List')
+
+@section('content')
+    <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 ">
+        {{-- <livewire:admin.products-table /> --}}
+    </div>
+@endsection
