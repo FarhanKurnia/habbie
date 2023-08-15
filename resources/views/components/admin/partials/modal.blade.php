5ec6a19a466cad1245243894f22f3655def04c13
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-15 23:40:42 +0700

    feat: change route name from props

--- resources/views/components/admin/partials/modal.blade.php
+++ resources/views/components/admin/partials/modal.blade.php
@@ -4,7 +4,7 @@
         <h3 class="font-bold text-lg">{{ $title }}</h3>
         <p class="py-4">{{ $description }}</p>
         <div class="modal-action">
-            <a class="btn bg-danger bg-opacity-75 hover:bg-danger text-white" href="{{ route('deleteProducts', $slug) }}">{{$action}}</a>
+            <a class="btn bg-danger bg-opacity-75 hover:bg-danger text-white" href="{{ route($routeName, $slug) }}">{{$action}}</a>
             <button class="btn">Close</button>
         </div>
     </form>
