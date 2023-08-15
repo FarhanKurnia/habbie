f9e61af0113e6eb77751036d15196ec13228f9c3
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-15 23:49:08 +0700

    feat: add livewire product category table

--- resources/views/pages/admin/products/categories/index.blade.php
+++ resources/views/pages/admin/products/categories/index.blade.php
@@ -4,6 +4,6 @@
 
 @section('content')
     <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 ">
-        {{-- <livewire:admin.products-table /> --}}
+        <livewire:admin.products-category-table />
     </div>
 @endsection
