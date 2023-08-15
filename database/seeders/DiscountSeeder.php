5e25044a130c41a61179d0f038c88bbab63b66e4
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-15 21:41:05 +0700

    fix: remove "%" in seeder

--- database/seeders/DiscountSeeder.php
+++ database/seeders/DiscountSeeder.php
@@ -13,17 +13,17 @@ class DiscountSeeder extends Seeder
     public function run(): void
     {
         DB::table('discounts')->insert([[
-            'name' => 'Discount 12%',
+            'name' => 'Discount 12',
             'rule' => 12,
-            'slug' => 'discount-12%',
+            'slug' => 'discount-12',
             'status' => 'active',
             'description'=>'Kisaran Diskon 1 Botol Telon = 12% (Reguler dan acak pada produk yang terpilih untuk didiskon) SELECTED ITEM/Season bisa berubah sewaktu-waktu.',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ],
         [
-            'name' => 'Discount 13%',
-            'slug' => 'discount-13%',
+            'name' => 'Discount 13',
+            'slug' => 'discount-13',
             'rule' => 13,
             'status' => 'active',
             'description'=>'Kisaran Diskon 1 Botol Cajuput Oil = 13% (Reguler dan acak pada produk yang terpilih untuk didiskon) SELECTED ITEM/Season bisa berubah sewaktu-waktu.',
@@ -31,8 +31,8 @@ class DiscountSeeder extends Seeder
             'updated_at' => Carbon::now(),
         ],
         [
-            'name' => 'Discount 18%',
-            'slug' => 'discount-18%',
+            'name' => 'Discount 18',
+            'slug' => 'discount-18',
             'rule' => 18,
             'status' => 'active',
             'description'=>'Flash sale 1 Botol Telon 18% (opsional)--tanggal payday (25/28/30)//tanggal kembar (misal 1.1, 2.2,3.3, dst) SELECTED ITEM/Season bisa berubah sewaktu-waktu.',
@@ -40,8 +40,8 @@ class DiscountSeeder extends Seeder
             'updated_at' => Carbon::now(),
         ],
         [
-            'name' => 'Discount 16%',
-            'slug' => 'discount-16%',
+            'name' => 'Discount 16',
+            'slug' => 'discount-16',
             'rule' => 16,
             'status' => 'active',
             'description'=>'Flash sale 1 Botol Cajuput Oil 16% (opsional)--tanggal payday (25/28/30)//tanggal kembar (misal 1.1, 2.2,3.3, dst) SELECTED ITEM/Season bisa berubah sewaktu-waktu.',
