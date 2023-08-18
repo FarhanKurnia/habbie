6ed9cbe0fb30c83bc347ea30f1324b55401049f6
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-18 07:39:52 +0700

    feat: add height screen background style

--- resources/views/pages/admin/articles/index.blade.php
+++ resources/views/pages/admin/articles/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'List All Articles')
 
 @section('content')
-    <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 ">
+    <div class="mx-auto max-w-screen-2xl h-screen p-4 md:p-6 2xl:p-10 ">
         <livewire:admin.articles-table />
     </div>
 @endsection
