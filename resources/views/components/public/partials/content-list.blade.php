a5e35253282924c8b7e1d7314fc5e1c5a6d9df39
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-18 07:38:50 +0700

    fix: show raw html on description

--- resources/views/components/public/partials/content-list.blade.php
+++ resources/views/components/public/partials/content-list.blade.php
@@ -10,7 +10,7 @@
         </div>
         <div>
             <h3 class="text-2xl font-bold text-pink-primary lg:py-4">{{ $offer['name'] }}</h3>
-            <p>{{ $offer['description'] }}</p>
+            <p>{!! $offer['description'] !!}</p>
             <a href="{{ $offer['status'] === 'non-active' ? '#' : url('/products/'. $offer->product['slug']) }}">
                 <button {{ $offer['status'] === 'non-active' ? 'disabled' : '' }} class="btn btn-primary rounded-full font-bold text-white my-6">Shop Now</button>
             </a>
