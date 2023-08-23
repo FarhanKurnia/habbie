c04a7877d46bd219b1a199d65980c93b44fab6ba
bagusgandhi <bagusgandhi4@gmail.com>
2023-08-24 00:04:51 +0700

    feat: get name from auth

--- resources/views/components/admin/template/navbar.blade.php
+++ resources/views/components/admin/template/navbar.blade.php
@@ -1,12 +1,12 @@
 <header class="sticky top-0 z-20 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none navbar">
     <div class="navbar-start">
-        <input type="text" placeholder="Type to search..." class="w-full bg-transparent pr-4 pl-9 focus:outline-none" />
+        {{-- <input type="text" placeholder="Type to search..." class="w-full bg-transparent pr-4 pl-9 focus:outline-none" /> --}}
     </div>
     <div class="navbar-end">
         <ul class="menu menu-horizontal px-1">
             <li tabindex="0">
               <details>
-                <summary>John Doe</summary>
+                <summary>{{Auth::user()->name}}</summary>
                 <ul class="p-2">
                   <li><a>Profile</a></li>
                   <li><a>Logout</a></li>
