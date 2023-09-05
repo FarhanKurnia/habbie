fcba19dcc67aba35b90caa09bfabf00ce3264a16
bagusgandhi <bagusgandhi4@gmail.com>
2023-09-05 21:06:53 +0700

    feat: adding livewire search component

--- resources/views/components/public/template/header.blade.php
+++ resources/views/components/public/template/header.blade.php
@@ -105,18 +105,7 @@
                 {{-- desktop cart info & log in --}}
                 <div class="hidden lg:flex gap-4 items-center">
                     {{-- search --}}
-                    <div class="relative w-42 ">
-                        <input type="text" placeholder="Search"
-                            class="w-full px-4 py-2 pr-12 rounded-full bg-grey-secondary-50 focus:outline-none" />
-                        <button class="absolute right-0 top-0 mt-1 mr-2 px-4 py-2 rounded-full  focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                            </svg>
-                        </button>
-
-                    </div>
+                    <livewire:search />
 
                     {{-- login desktop --}}
                     <div class="dropdown dropdown-hover">
@@ -138,6 +127,7 @@
                         @else
                             <ul tabindex="0"
                                 class="dropdown-content z-[20] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a href="{{ route('profile') }}">Profile</a></li>
                                 <li><a href="{{ url('invoice') }}">Invoice</a></li>
                                 <li><a href="{{ url('/logout') }}">Logout</a></li>
 
