3b44c186dea807ef03f4011c31f5f9126d24fff1
bagusgandhi <bagusgandhi4@gmail.com>
2023-09-05 21:07:45 +0700

    fix: remove h-screen class

--- resources/views/pages/admin/products/index.blade.php
+++ resources/views/pages/admin/products/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Product List')
 
 @section('content')
-    <div class="mx-auto max-w-screen-2xl h-screen p-4 md:p-6 2xl:p-10 ">
+    <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 ">
         <livewire:admin.products-table />
     </div>
 @endsection
