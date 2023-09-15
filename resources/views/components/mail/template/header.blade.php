dba98e1005bd65046ccd1ded54433831f6f8eb5b
bagusgandhi <bagusgandhi4@gmail.com>
2023-09-15 23:10:41 +0700

    feat: header template email

---
+++ resources/views/components/mail/template/header.blade.php
@@ -0,0 +1,9 @@
+<div class="flex flex-col space-y-4">
+    <div class="w-36 mx-auto pt-2">
+        <a href="{{ url('/') }}" target="_blank"><img src="{{ asset('storage/img/logo/logo-habbie.svg') }}"
+                alt="Habbie Aromatic by Nature All Around the world" ></a>
+    </div>
+    <span class="p-2 flex justify-center gap-4">
+        <p>Aromatic by Nature All Around the World</p>
+    </span>
+</div>
