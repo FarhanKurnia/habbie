d70a3b0372d1ae790d3b2ed0b3c2a2735cd16a8a
bagusgandhi <bagusgandhi4@gmail.com>
2023-09-15 23:10:14 +0700

    feat: footer template mail

---
+++ resources/views/components/mail/template/footer.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <div class="grid grid-cols-2 text-white">
+        <div>
+            <ul class="text-sm">
+                <li><a class="hover:text-pink-primary" href="">Our Products</a></li>
+                <li><a class="hover:text-pink-primary"  href="">Join Membership</a></li>
+                <li><a class="hover:text-pink-primary"  href="">Offers</a></li>
+                <li><a class="hover:text-pink-primary"  href="">Blog</a></li>
+            </ul>
+        </div>
+        <div>
+            <p class="text-sm">Alamat: </p>
+            <p class="text-sm">Email: </p>
+            <p class="text-sm">Phone / WA: </p>
+        </div>
+    </div>
+
+    <div>
+
+    </div>
+</div>
