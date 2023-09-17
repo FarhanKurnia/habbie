46785f6df8876f2e6273bdf33fdf1b86b3ebf58f
bagusgandhi <bagusgandhi4@gmail.com>
2023-09-17 22:22:52 +0700

    feat: link href cdn font

--- resources/views/layouts/base-email-layout.blade.php
+++ resources/views/layouts/base-email-layout.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+    <link href="https://fonts.cdnfonts.com/css/dinpro-medium" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/dinpro-bold" rel="stylesheet">
     @vite('resources/css/app.css')
     <title>@yield('title')</title>
 </head>
 
 <body class="bg-grey-secondary-50 px-4">
-    <div class="w-1/3 mx-auto">
+    <div class="w-1/2 mx-auto">
         <header class="container bg-pink-bloosom py-4 bg-opacity-60">
             @include('components.mail.template.header')
         </header>
