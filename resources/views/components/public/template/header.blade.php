77b41193d7bb575e57c95221838d9723b7bd8aea
bagusgandhi <bagusgandhi4@gmail.com>
2023-10-07 06:56:43 +0700

    feat: change mobile menu button and content to livewire component

--- resources/views/components/public/template/header.blade.php
+++ resources/views/components/public/template/header.blade.php
@@ -9,45 +9,7 @@
             <div class="navbar-start lg:w-full">
                 <div class="mobile flex items-center lg:hidden">
                     {{-- mobile menu --}}
-                    <div class="dropdown dropdown-start">
-                        <label tabindex="0" class="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
-                        </label>
-                        <ul tabindex="0"
-                            class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-ful z-20">
-                            <li><a href="{{ url('/offers') }}">Offers</a></li>
-                            <li>
-                                <a>Products</a>
-                                <livewire:menu-category-product />
-                            </li>
-                            <li><a href="{{ url('testimonials') }}">Testimonial</a></li>
-                            <li>
-                                <a>Membership</a>
-                                <ul class="flex flex-col lg:flex-row p-2 z-20">
-                                    <li>
-                                        <ul class="flex flex-col">
-                                            <li><a href="{{ url('membership') }}">Information</a></li>
-                                            <li><a href="{{ url('membership/join') }}">Join Program</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="{{ url('media') }}">Media</a></li>
-                            @if (is_null(Auth::user()))
-                                <li><a href="{{ url('/login') }}">Login</a></li>
-                                <li><a href="{{ url('/register') }}">Register</a></li>
-                            @else
-                                {{-- <li><a href="{{ url('/invoice') }}">Invoice</a></li> --}}
-                                <li><a href="{{ url('/invoice') }}">Invoice</a></li>
-                                <li><a href="{{ url('/logout') }}">Logout</a></li>
-                            @endif
-                        </ul>
-                    </div>
-
+                    <livewire:mobile-menu-button />
                 </div>
 
                 {{-- logo desktop --}}
@@ -58,31 +20,39 @@
                 </div>
 
                 {{-- menu desktop --}}
-                <ul class="menu menu-horizontal hidden lg:flex">
-                    <li><a href="{{ url('/offers') }}" class="text-sm font-bold">OFFERS</a></li>
-                    <li tabindex="0">
-                        <details>
-                            <summary class="text-sm font-bold">PRODUCT</summary>
-                            <livewire:menu-category-product />
-                        </details>
-                    </li>
-                    <li><a href="{{ url('testimonials') }}" class="text-sm font-bold">TESTIMONIAL</a></li>
-                    <li tabindex="0">
-                        <details>
-                            <summary class="text-sm font-bold">MEMBERSHIP</summary>
-                            <ul class="flex flex-col lg:flex-row p-2 z-20">
-                                <li>
-                                    <ul class="flex flex-col">
-                                        <li><a href="{{ url('membership') }}">Information</a></li>
-                                        <li><a href="{{ url('membership/join') }}">Join Program</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </details>
-                    </li>
+                <div class="menu menu-horizontal hidden lg:flex">
+                    <div><a href="{{ url('/offers') }}" class="btn font-bold bg-white border-0">OFFERS</a></div>
+                    <div class="dropdown dropdown-hover">
+                        <label tabindex="0" class="btn bg-white border-0 flex gap-2 items-center">
+                            <p class="font-bold">Product</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="2.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                            </svg>
+
+                        </label>
+                        <livewire:menu-category-product />
+                    </div>
+                    <div><a href="{{ url('testimonials') }}" class="btn font-bold bg-white border-0">TESTIMONIAL</a>
+                    </div>
+                    <div class="dropdown dropdown-hover">
+                        <label tabindex="0" class="btn bg-white border-0 flex gap-2 items-center">
+                            <p class="font-bold">Membership</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="2.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                            </svg>
+
+                        </label>
+                        <ul tabindex="0" class="menu dropdown-content z-50 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a href="{{ url('membership') }}">Information</a></li>
+                            <li><a href="{{ url('membership/join') }}">Join Program</a></li>
+                        </ul>
+                    </div>
+
                     {{-- <li><a class="text-sm font-bold">MEMBERSHIP</a></li> --}}
-                    <li><a href="{{ url('media') }}" class="text-sm font-bold">MEDIA</a></li>
-                </ul>
+                    <div><a href="{{ url('media') }}" class="btn font-bold bg-white border-0">MEDIA</a></div>
+                </div>
 
             </div>
 
@@ -95,20 +65,17 @@
                 </div>
             </div>
 
-            <div class="navbar-end">
-                {{-- mobile --}}
-                {{-- <div class="lg:hidden">
-                    <livewire:cart-info />
-                </div> --}}
+            {{-- mobile --}}
+            <div class="navbar-end lg:hidden">
+                <livewire:cart-info />
+            </div>
 
+            <div class="navbar-end hidden lg:flex gap-4">
 
-                {{-- desktop cart info & log in --}}
-                <div class="hidden lg:flex gap-4 items-center">
-                    {{-- search --}}
+                <div>
                     <livewire:search />
 
-                    {{-- login desktop --}}
-                    <div class="hidden lg:flex dropdown dropdown-hover">
+                    <div class="dropdown dropdown-hover dropdown-end">
                         <label tabindex="0" class="text-sm font-bold">
                             @if (is_null(Auth::user()))
                                 ACCOUNT
@@ -117,7 +84,6 @@
                             @endif
 
                         </label>
-                        {{-- {{Auth::user()}} --}}
                         @if (is_null(Auth::user()))
                             <ul tabindex="0"
                                 class="dropdown-content z-[20] menu p-2 shadow bg-base-100 rounded-box w-52">
@@ -135,13 +101,21 @@
                         @endif
                     </div>
 
-                    {{-- cart desktop --}}
                     <livewire:cart-info />
-                </div>
 
+                </div>
             </div>
         </div>
     </div>
+
+    {{-- dropdown mobile --}}
+    <livewire:mobile-menu />
+    @if (session()->has('error'))
+        <div class="mx-auto">
+            <livewire:alert :message="session('error')" :background="'bg-danger'" />
+        </div>
+    @endif
+
     <div class="container mx-auto">
         <div class="relative">
             <div class="absolute w-full top-0 ">
@@ -149,4 +123,5 @@
             </div>
         </div>
     </div>
+
 </header>
