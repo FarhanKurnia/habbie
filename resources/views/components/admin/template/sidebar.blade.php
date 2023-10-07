903dd1549269bcacf00348c35daee76a5b90d389
bagusgandhi <bagusgandhi4@gmail.com>
2023-10-07 10:33:33 +0700

    fix: remove dark style

--- resources/views/components/admin/template/sidebar.blade.php
+++ resources/views/components/admin/template/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside
-    class="hidden absolute left-0 top-0 z-20 lg:flex h-screen w-72.5 flex-col overflow-y-hidden bg-pink-primary duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0">
+    class="hidden absolute left-0 top-0 z-20 lg:flex h-screen w-72.5 flex-col overflow-y-hidden bg-pink-primary duration-300 ease-linear lg:static lg:translate-x-0">
     {{-- Sidebar Header --}}
     <div class="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <a href="{{ url('admin') }}">
